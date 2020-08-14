33dd35ca620c242eecd44e9c31d25e543790f68b
Akin Ajayi <wiajayi@gmail.com>
2020-08-14 23:08:03 +0200

    Start of Covid Algorithim, Rough copy to be prepped and clean

--- Covid.py
+++ Covid.py
@@ -1,38 +1,44 @@
 import pandas as pd
 import requests
-import json
+import folium
+from flask import Flask,render_template
+import datetime
+
 
 # API Call to collect the Covid Data Set, Convert into Json format and put into a dataframe.
 # Set the display option to max to see all columns
 #Grouped the data by country
-# def find_top_confirmed(n=15):
-re = requests.get("https://api.thevirustracker.com/free-api?countryTotals=ALL")
+
+re = requests.get("https://corona.lmao.ninja/v2/countries?yesterday&sort")
 re = re.json()
-re = re['countryitems']
-# print(re)
-df = pd.DataFrame(re)
-print(df)
+re = pd.DataFrame.from_dict(re)
+pd.set_option('display.max_columns', None)
+country_info = re['countryInfo']
+country_info = pd.json_normalize(country_info)
+df = pd.concat([re, country_info], axis=1,sort=False)
+df = df.drop('countryInfo', axis=1)
 
+def find_largest_cases(n):
+    by_country = df.groupby('country').sum()[['cases', 'deaths', 'recovered', 'active']]
+    covid_df = by_country.nlargest(n, 'cases')
+    return covid_df
+# print(find_largest_cases(5))
 
-# re = pd.DataFrame.from_dict(re)
-# re = pd.DataFrame(re)
-# re = re.T
-# print(re)
-
-# print(re)
-
-
-
-
-# covid_df = pd.DataFrame(data)
-# pd.set_option('display.max_columns', None)
-# print(covid_df.head())
-# by_country = covid_df.groupby('Country').sum()['Deaths']
-# print(by_country)
-
-# l_cases = by_country.nlargest(n, 'Confirmed')
-# return l_cases,
-
-# print(find_top_confirmed())
-
-
+# m = folium.Map(location=(37.76, -122.45), zoom_start=12)
+#
+# def circle_maker(x):
+#     folium.Circle(location=[x[0],x[1]],
+#                   radius=float(x[2])* 10,
+#                   color="red",
+#                   popup='{}\n confirmed cases: {}'.format((x[3],x[2])).add_to(m))
+#
+# df[['lat','long','cases']].apply(lambda x: circle_maker(x), axis=1)
+# html_map = m._repr_html()
+#
+# app = Flask(__name__)
+# @app.route('/')
+# def home():
+#     return render_template("home.html", table=df, cmap=html_map, pairs=pairs)
+#
+# if __name__ == "__main__":
+#     app.run(debug=True)
