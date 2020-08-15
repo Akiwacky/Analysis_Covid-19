6e46f89ff79bd6099023baf96be4feda573e31d8
Akin Ajayi <wiajayi@gmail.com>
2020-08-15 18:01:34 +0200

    Python analysis complete and data visualisation in folium is done

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,9 @@
 	style.css,4/6/46e3381e9569c09e85a30b9e5a2af7525ee9fd78
 8
 Tests.py,a/b/ab299033bb0e267f32fe5b4b4f672d309ffef689
+D
+world-countries.json,6/c/6c093ebca6d3a1e905648d884cef7f444370e835
+?
+covid_data.html,f/2/f2c34121aa7d2eb376837b63ea97c229f4864183
+D
+Covid Analysis.ipynb,6/c/6cdb8950e3fe1c35341a5f0a3c04df4b02807d75
--- Covid.py
+++ Covid.py
@@ -1,15 +1,10 @@
 import pandas as pd
 import requests
-import os
-import folium
-from flask import Flask,render_template
-import datetime
-
 
 # API Call to collect the Covid Data Set, Convert into Json format and put into a dataframe.
 # Set the display option to max to see all columns
 #Grouped the data by country
-# country_geo = '{url}/us-states.json'
+country_geo = 'world-countries.json'
 re = requests.get("https://corona.lmao.ninja/v2/countries?yesterday&sort")
 re = re.json()
 re = pd.DataFrame.from_dict(re)
@@ -23,47 +18,5 @@ def find_largest_cases(n):
     by_country = df.groupby('country').sum()[['cases', 'deaths', 'recovered', 'active']]
     covid_df = by_country.nlargest(n, 'cases')
     return covid_df
-# print(find_largest_cases(5))
 
-m = folium.Map(
-    location=[100, 0],
-    zoom_start=3,
-    tiles='Stamen Toner'
-)
 
-folium.Choropleth (
-    geo_data= 'geo.json',
-    data=df,
-    columns=['lat', 'long'],
-    key_on='feature.id',
-    fill_color='YlGnBu',
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name='Test'
-).add_to(m)
-
-popup = 'Test'
-
-folium.CircleMarker(
-    location=[48, -102],
-    radius=10,
-    fill=True,
-    popup=popup,
-    weight=1,
-).add_to(m)
-
-m.save(os.path.join('results', 'CheckZorder.html'))
-
-m
-
-#
-# df[['lat','long','cases']].apply(lambda x: circle_maker(x), axis=1)
-# html_map = m._repr_html()
-#
-# app = Flask(__name__)
-# @app.route('/')
-# def home():
-#     return render_template("home.html", table=df, cmap=html_map, pairs=pairs)
-#
-# if __name__ == "__main__":
-#     app.run(debug=True)
